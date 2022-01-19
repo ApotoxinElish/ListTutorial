d0aa786ef7aeb6bbff1b7ab195ef62710882f6ac
ApotoxinElish <19079181d@connect.polyu.hk>
2022-01-19 14:15:50 +0800

    Update

---
+++ Assets/CardDataBase.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDataBase : MonoBehaviour
+{
+    public static List<Card> cardList = new List<Card>();
+
+    void Awake()
+    {
+        cardList.Add(new Card(0, "None", 0, 0, "None"));
+        cardList.Add(new Card(1, "Elf", 2, 1000, "It's Elf"));
+        cardList.Add(new Card(2, "Dwarf", 3, 3000, "It's Dwarf"));
+        cardList.Add(new Card(3, "Human", 5, 6000, "It's Human"));
+        cardList.Add(new Card(4, "Demon", 1, 1000, "It's Demon"));
+    }
+}
